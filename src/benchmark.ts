745ec1301096050eb0154b3d7f4c69f20323fe61
bbaresic <153560835+bbaresic@users.noreply.github.com>
2024-04-29 15:29:46 -0500

    Update formatting

--- src/benchmark.ts
+++ src/benchmark.ts
@@ -1,7 +1,7 @@
 import cloneDeep from "clone-deep";
 import { copy as copyAnything } from "copy-anything";
-import deepEqual from "fast-deep-equal";
 import fastCopy from "fast-copy";
+import deepEqual from "fast-deep-equal";
 import justClone from "just-clone";
 // @ts-ignore
 import justClonePrebid from "just-clone-prebid";
@@ -15,127 +15,127 @@ import { bidRequest } from "./bidRequest.js";
 
 const bench = new Bench({ time: 1000, iterations: 100 });
 bench
-  .add("ramda", () => {
-    ramdaClone(bidRequest);
-  })
-  .add("lodash CloneDeep", () => {
-    lodashCloneDeep(bidRequest);
-  })
-  .add("cloneDeep", () => {
-    cloneDeep(bidRequest);
-  })
-  .add("fastCopy", () => {
-    fastCopy(bidRequest);
-  })
-  .add("nanoCopy", () => {
-    nanoCopy(bidRequest);
-  })
-  .add("just-clone (v6)", () => {
-    justClone(bidRequest);
-  })
-  .add("just-clone prebid version (v1)", () => {
-    justClonePrebid(bidRequest);
-  })
-  .add("klona (json)", () => {
-    klona(bidRequest);
-  })
-  .add("copyAnything", () => {
-    copyAnything(bidRequest);
-  })
-  .add("json-parse-stringify", () => {
-    JSON.parse(JSON.stringify(bidRequest));
-  })
-  .add("structured-clone", () => {
-    structuredClone(bidRequest);
-  });
+	.add("ramda", () => {
+		ramdaClone(bidRequest);
+	})
+	.add("lodash CloneDeep", () => {
+		lodashCloneDeep(bidRequest);
+	})
+	.add("cloneDeep", () => {
+		cloneDeep(bidRequest);
+	})
+	.add("fastCopy", () => {
+		fastCopy(bidRequest);
+	})
+	.add("nanoCopy", () => {
+		nanoCopy(bidRequest);
+	})
+	.add("just-clone (v6)", () => {
+		justClone(bidRequest);
+	})
+	.add("just-clone prebid version (v1)", () => {
+		justClonePrebid(bidRequest);
+	})
+	.add("klona (json)", () => {
+		klona(bidRequest);
+	})
+	.add("copyAnything", () => {
+		copyAnything(bidRequest);
+	})
+	.add("json-parse-stringify", () => {
+		JSON.parse(JSON.stringify(bidRequest));
+	})
+	.add("structured-clone", () => {
+		structuredClone(bidRequest);
+	});
 
 (async () => {
-  console.log("Starting benchmark...");
+	console.log("Starting benchmark...");
 
-  await bench.warmup();
-  await bench.run();
+	await bench.warmup();
+	await bench.run();
 
-  //  check if all of the libraries are working
-  function testDeepCopy() {
-    const libraries = {
-      ramda: ramdaClone,
-      lodashCloneDeep: lodashCloneDeep,
-      cloneDeep: cloneDeep,
-      fastCopy: fastCopy,
-      nanoCopy: nanoCopy,
-      justClone: justClone,
-      klona: klona,
-      copyAnything: copyAnything,
-      "json-parse-stringify": () => JSON.parse(JSON.stringify(bidRequest)),
-      "structured-clone": () => structuredClone(bidRequest),
-    };
+	//  check if all of the libraries are working
+	function testDeepCopy() {
+		const libraries = {
+			ramda: ramdaClone,
+			lodashCloneDeep: lodashCloneDeep,
+			cloneDeep: cloneDeep,
+			fastCopy: fastCopy,
+			nanoCopy: nanoCopy,
+			justClone: justClone,
+			klona: klona,
+			copyAnything: copyAnything,
+			"json-parse-stringify": () => JSON.parse(JSON.stringify(bidRequest)),
+			"structured-clone": () => structuredClone(bidRequest),
+		};
 
-    const results = [];
+		const results = [];
 
-    for (const [name, func] of Object.entries(libraries)) {
-      const copy = func(bidRequest);
-      try {
-        deepEqual(copy, bidRequest);
-        results.push({ Library: name, Result: "✅" });
-      } catch (error) {
-        results.push({ Library: name, Result: "❌" });
-      }
-    }
+		for (const [name, func] of Object.entries(libraries)) {
+			const copy = func(bidRequest);
+			try {
+				deepEqual(copy, bidRequest);
+				results.push({ Library: name, Result: "✅" });
+			} catch (error) {
+				results.push({ Library: name, Result: "❌" });
+			}
+		}
 
-    console.log("Deep copy is copying properly:");
-    console.table(results);
-  }
+		console.log("Deep copy is copying properly:");
+		console.table(results);
+	}
 
-  testDeepCopy();
+	testDeepCopy();
 
-  // format the results
-  const sortedTable = bench.table().sort((a, b) => {
-    if (
-      a &&
-      b &&
-      a["ops/sec"] &&
-      b["ops/sec"] &&
-      typeof a["ops/sec"] === "string" &&
-      typeof b["ops/sec"] === "string"
-    ) {
-      return (
-        Number(b["ops/sec"].replace(/,/g, "")) -
-        Number(a["ops/sec"].replace(/,/g, ""))
-      );
-    }
-    return 0;
-  });
+	// format the results
+	const sortedTable = bench.table().sort((a, b) => {
+		if (
+			a &&
+			b &&
+			a["ops/sec"] &&
+			b["ops/sec"] &&
+			typeof a["ops/sec"] === "string" &&
+			typeof b["ops/sec"] === "string"
+		) {
+			return (
+				Number(b["ops/sec"].replace(/,/g, "")) -
+				Number(a["ops/sec"].replace(/,/g, ""))
+			);
+		}
+		return 0;
+	});
 
-  // Print the results
-  console.table(sortedTable);
+	// Print the results
+	console.table(sortedTable);
 
-  //   if in browser display the results in a table
-  if (typeof document !== "undefined") {
-    // Create a table and a table head
-    let tableHTML = `
+	//   if in browser display the results in a table
+	if (typeof document !== "undefined") {
+		// Create a table and a table head
+		let tableHTML = `
 	<table>
 		<thead>
 			<tr>
 				${Object.keys(sortedTable[0] || {})
-          .map((key) => `<th>${key}</th>`)
-          .join("")}
+					.map((key) => `<th>${key}</th>`)
+					.join("")}
 			</tr>
 		</thead>
 		<tbody>
 `;
 
-    // Create table rows for each object in sortedTable
-    for (const row of sortedTable) {
-      tableHTML += `
+		// Create table rows for each object in sortedTable
+		for (const row of sortedTable) {
+			tableHTML += `
 		<tr>
 			${Object.values(row || {})
-        .map((val) => `<td>${val}</td>`)
-        .join("")}
+				.map((val) => `<td>${val}</td>`)
+				.join("")}
 		</tr>
 	`;
-    }
+		}
 
-    // Insert the table into the HTML document
-    document.body.innerHTML = tableHTML;
-  }
+		// Insert the table into the HTML document
+		document.body.innerHTML = tableHTML;
+	}
 })();
